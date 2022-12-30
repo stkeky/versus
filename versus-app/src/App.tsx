3ee5bf88719d7728908a45c04a17f4e18f7e2c82
Stefan Ristanović <st.keky@gmail.com>
2022-12-31 00:14:27 +0100

    Update home

--- versus-app/src/App.tsx
+++ versus-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
     DesktopOutlined,
     FileOutlined,
@@ -6,10 +6,11 @@ import {
     TeamOutlined,
     UserOutlined,
 } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import type {MenuProps} from 'antd';
+import {Typography, Skeleton, Layout, Menu, theme} from 'antd';
 
-const { Header, Content, Footer, Sider } = Layout;
+const {Header, Content, Footer, Sider} = Layout;
+const { Text, Link } = Typography;
 
 type MenuItem = Required<MenuProps>['items'][number];
 
@@ -28,41 +29,41 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-    getItem('Option 1', '1', <PieChartOutlined />),
-    getItem('Option 2', '2', <DesktopOutlined />),
-    getItem('User', 'sub1', <UserOutlined />, [
+    getItem('Option 1', '1', <PieChartOutlined/>),
+    getItem('Option 2', '2', <DesktopOutlined/>),
+    getItem('User', 'sub1', <UserOutlined/>, [
         getItem('Tom', '3'),
         getItem('Bill', '4'),
         getItem('Alex', '5'),
     ]),
-    getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
-    getItem('Files', '9', <FileOutlined />),
+    getItem('Team', 'sub2', <TeamOutlined/>, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+    getItem('Files', '9', <FileOutlined/>),
 ];
 
 const App: React.FC = () => {
     const [collapsed, setCollapsed] = useState(false);
     const {
-        token: { colorBgContainer },
+        token: {colorBgContainer},
     } = theme.useToken();
 
     return (
-        <Layout style={{ minHeight: '100vh' }}>
+        <Layout style={{minHeight: '100vh'}}>
             <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
-                <div style={{ height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)' }} />
-                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+                <div style={{height: 32, margin: 16, background: 'rgba(255, 255, 255, 0.2)'}}/>
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items}/>
             </Sider>
             <Layout className="site-layout">
-                <Header style={{ padding: 0, background: colorBgContainer }} />
-                <Content style={{ margin: '0 16px' }}>
-                    <Breadcrumb style={{ margin: '16px 0' }}>
-                        <Breadcrumb.Item>User</Breadcrumb.Item>
-                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                    </Breadcrumb>
-                    <div style={{ padding: 24, minHeight: 360, background: colorBgContainer }}>
-                        Bill is a cat.
+                <Header style={{padding: 0, background: colorBgContainer}}/>
+                <Content style={{margin: '0 16px'}}>
+                    <Skeleton active/>
+                    <div style={{padding: 24, minHeight: 360, background: colorBgContainer}}>
+                        <Skeleton active/>
                     </div>
                 </Content>
-                <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                <Footer style={{textAlign: 'center'}}>
+                    <Text strong>Versus ©2022 ✨</Text>
+                    <Link href="https://github.com/stkeky" target="_blank">@stkeky</Link>
+                </Footer>
             </Layout>
         </Layout>
     );
