caf7f063f336727822491dbd7920d0c9b82fceaa
Stefan Ristanović <st.keky@gmail.com>
2023-01-02 23:46:13 +0100

    Make heatmap work

--- versus-app/src/components/Snooze.tsx
+++ versus-app/src/components/Snooze.tsx
@@ -6,10 +6,24 @@ import {buildStyles, CircularProgressbar} from "react-circular-progressbar";
 import 'react-circular-progressbar/dist/styles.css';
 import 'react-calendar-heatmap/dist/styles.css';
 import CalendarHeatmap from 'react-calendar-heatmap';
-
+import ReactTooltip from 'react-tooltip';
 
 const Snooze: React.FC = () => {
+    const stkekyRawPoints = data.timeline.reduce((sum, current) => sum + current.stkeky.points, 0);
+    const snoozeRawPoints = data.timeline.reduce((sum, current) => sum + current.snooze.points, 0);
+    const stkekySupporters = data.timeline.reduce((sum, current) => sum + current.stkeky.supporters.length, 0);
+    const snoozeSupporters = data.timeline.reduce((sum, current) => sum + current.snooze.supporters.length, 0);
+    const stkekyTotal = stkekyRawPoints + stkekySupporters
+    const snoozeTotal = snoozeRawPoints + snoozeSupporters
 
+    const stkekySupportersList = data.timeline.map(x => x.stkeky.supporters).reduce((all, current) => all.concat(current), [])
+    const snoozeSupportersList = data.timeline.map(x => x.snooze.supporters).reduce((all, current) => all.concat(current), [])
+
+    const heatmap = data.timeline.map(x => ({"date": new Date(x.date) , "count": x.stkeky.points + x.stkeky.supporters.length - x.snooze.points - x.snooze.supporters.length}))
+
+    // @ts-ignore
+    // @ts-ignore
+    // @ts-ignore
     return (
         <Fragment>
             <div style={{padding: 24, minHeight: "100%"}}>
@@ -35,7 +49,7 @@ const Snooze: React.FC = () => {
                                 </Card.Content>
                                 <Card.Content extra>
                                     <Icon name='user'/>
-                                    {data.stkeky.supporters.length} Supporters
+                                    {stkekySupporters} Supporters
                                 </Card.Content>
                             </Card>
                         </Grid.Column>
@@ -44,12 +58,12 @@ const Snooze: React.FC = () => {
                             <Grid.Row>
                                 <Grid.Column width={8}>
                                     <Segment>
-                                        <CircularProgressbar value={data.stkeky.progress} maxValue={100} text={`${data.stkeky.progress} Points`} />
+                                        <CircularProgressbar value={stkekyTotal} maxValue={100} text={`${stkekyTotal} Points`} />
                                     </Segment>
                                 </Grid.Column>
                                 <Grid.Column width={8}>
                                     <Segment>
-                                        <CircularProgressbar value={data.snooze.progress} maxValue={100} text={`${data.snooze.progress} Points`} styles={buildStyles({pathColor: `rgba(196, 40, 36, 0.8)`, textColor: `rgba(196, 40, 36, 0.8)`})} />
+                                        <CircularProgressbar value={snoozeTotal} maxValue={100} text={`${snoozeTotal} Points`} styles={buildStyles({pathColor: `rgba(196, 40, 36, 0.8)`, textColor: `rgba(196, 40, 36, 0.8)`})} />
                                     </Segment>
                                 </Grid.Column>
                             </Grid.Row>
@@ -57,14 +71,31 @@ const Snooze: React.FC = () => {
                                 <Grid.Column>
                                     <Segment>
                                         <CalendarHeatmap
-                                            startDate={new Date('2023-01-01')}
-                                            endDate={new Date('2023-12-31')}
-                                            values={[
-                                                { date: '2023-01-01', count: 12 },
-                                                { date: '2023-01-02', count: 122 },
-                                                { date: '2023-01-03', count: 38 },
-                                            ]}
+
+                                            startDate={new Date('2022-12-31')}
+                                            endDate={new Date('2023-12-30')}
+                                            values={heatmap}
+
+                                            tooltipDataAttrs={(value:any) => {
+                                                return {
+                                                    'data-tip': value.count
+                                                    }
+                                                }
+                                            }
+
+                                            classForValue={(value) => {
+                                                if (!value) {
+                                                    return 'color-empty';
+                                                }
+                                                if (value.count > 0) {
+                                                    return `color-gitlab-3`;
+                                                } else {
+                                                    return `color-github-4`;
+                                                }
+                                            }}
+
                                         />
+                                        <ReactTooltip/>
                                     </Segment>
                                 </Grid.Column>
 
@@ -89,7 +120,7 @@ const Snooze: React.FC = () => {
                                 </Card.Content>
                                 <Card.Content extra>
                                     <Icon name='user'/>
-                                    {data.snooze.supporters.length} Supporters
+                                    {snoozeSupporters} Supporters
                                 </Card.Content>
                             </Card>
                         </Grid.Column>
@@ -97,7 +128,7 @@ const Snooze: React.FC = () => {
                     <Grid.Row>
                         <Grid.Column width={7}>
                             <List>
-                                {data.stkeky.supporters.map((supporter: any) => {
+                                {stkekySupportersList.map((supporter: any) => {
                                     return (
                                         <List.Item>
                                             <Image avatar src={supporter.avatar}/>
@@ -120,7 +151,7 @@ const Snooze: React.FC = () => {
                         </Grid.Column>
                         <Grid.Column width={7}>
                             <List>
-                                {data.snooze.supporters.map((supporter: any) => {
+                                {snoozeSupportersList.map((supporter: any) => {
                                     return (
                                         <List.Item>
                                             <Image avatar src={supporter.avatar}/>
